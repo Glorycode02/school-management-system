ac2d23300f1adeb6edbb231e066318959f1ff90f
Glorycode02 <glorycode02@gmail.com>
2024-08-14 18:41:57 +0200

    commited

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -3,7 +3,6 @@ const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-
 const register = async (req, res) => {
   try {
     const { firstname, lastname, username, email, password, role } = req.body;
@@ -29,23 +28,29 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (!user) {
-    res.json({
-      msg: "User doesn't exist",
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ msg: "User doesn't exist" });
+    }
+
+    const passwordCheck = await bcrypt.compare(password, user.password);
+
+    if (!passwordCheck) {
+      return res.status(401).json({ msg: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
     });
-  }
-  const passwordCheck = await bcrypt.compare(password, user.password);
-  if (!passwordCheck) {
-    res.json({ msg: "Invalid credentials" });
-  }
 
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "1h",
-  });
-
-  res.status(200).json({ message: "Login successful", token });
+    // Return the user details along with the token
+    res.status(200).json({ message: "Login successful", token, user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 module.exports = {
